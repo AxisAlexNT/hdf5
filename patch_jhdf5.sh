a53a53e21779c6cd68f81e02e9fb7edca4dc4608
Aleksandr Serdiukov <ntwwwnt@gmail.com>
2023-10-17 18:34:58 +0300

    Removing old JNI subdirectory

--- patch_jhdf5.sh
+++ patch_jhdf5.sh
@@ -4,5 +4,6 @@ cd "${SCRIPT_DIR}"
 git clone https://sissource.ethz.ch/sispub/jhdf5.git
 pwd
 ls -alh
+rm -fR java/src/jni
 cp -af jhdf5/source/c/jni java/src/
 cp -af jhdf5/source/c/*.c java/src/jni/
