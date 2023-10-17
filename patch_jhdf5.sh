969875ada77c707a5360aeeb3b3f38110409adce
Aleksandr Serdiukov <ntwwwnt@gmail.com>
2023-10-17 17:33:28 +0300

    Added debug output of ls

--- patch_jhdf5.sh
+++ patch_jhdf5.sh
@@ -2,5 +2,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}"
 git clone https://sissource.ethz.ch/sispub/jhdf5.git
+pwd
+ls -alh
 cp -arf jhdf5/source/c/jni hdf5/java/src/
 cp -arf jhdf5/source/c/*.c hdf5/java/src/jni/
